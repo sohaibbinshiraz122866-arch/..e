c2c6f29a5528938485cfa11e3af986e48f978cf4
unknown <sohaibbinshiraz122866@gmail.com>
2026-02-08 20:07:53 +0600

    sections added

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
   $('.bannerSlider').slick({
     arrows: false,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 2000,
     dots: true,
     fade: true,
@@ -15,7 +15,24 @@ $(document).ready(function () {
   arrows: true,
   infinite: true,
   prevArrow: '.leftArrow',
-  nextArrow: '.rightArrow'
+  nextArrow: '.rightArrow',
+   responsive: [
+      {
+        breakpoint: 1024,
+        settings: { slidesToShow: 3,arrows: true }
+      },
+      {
+        breakpoint: 600,
+        settings: { slidesToShow: 2 }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
 });
 
 
@@ -26,12 +43,12 @@ $(document).ready(function () {
     arrows: true,
     autoplay: false,
     autoplaySpeed: 2000,
-    prevArrow: '.leftArrow',
-    nextArrow: '.rightArrow',
+    prevArrow: '#psLeftArrow',
+    nextArrow: '#psRightArrow',
     responsive: [
       {
         breakpoint: 1024,
-        settings: { slidesToShow: 3 }
+        settings: { slidesToShow: 3,arrows: true }
       },
       {
         breakpoint: 600,
@@ -46,5 +63,22 @@ $(document).ready(function () {
       }
     ]
   });
-
+ $('.client_slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: '#Client .clientArrow .prev',
+  nextArrow: '#Client .clientArrow .next',
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: { slidesToShow: 2 }
+    },
+    {
+      breakpoint: 576,
+      settings: { slidesToShow: 1 }
+    }
+  ]
+});
 });
